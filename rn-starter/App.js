5e3b9c02e8541e26dfdbe981a7bf0d1c316e22f8
Jagbir <birchaudhary2000@gmail.com>
2020-07-11 14:14:47 +0930

    second commit

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -2,11 +2,15 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import TextScreen from './src/screens/TextScreen';
+import MarriageWelcomeScreen from "./src/screens/MarriageWelcomeScreen";
+import CreateMarriagePlayersScreen from "./src/screens/CreateMarriagePlayersScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Text: TextScreen
+    Text: TextScreen,
+    MarriageWelcome: MarriageWelcomeScreen,
+    CreateMarriagePlayers: CreateMarriagePlayersScreen
   },
   {
     initialRouteName: "Home",
